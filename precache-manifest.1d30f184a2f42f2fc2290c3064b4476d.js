715c89a56cb9acced0168e561d5b613afdf3f3b0
Ben Swannack <ben.swannack@softwire.com>
2019-05-15 18:45:57 +0100

    Updates

---
+++ precache-manifest.1d30f184a2f42f2fc2290c3064b4476d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d55219e7988c0454e32",
+    "url": "/homepage/static/js/runtime~main.336ecad5.js"
+  },
+  {
+    "revision": "03664f08820fb46832d2",
+    "url": "/homepage/static/js/main.bb8bfb86.chunk.js"
+  },
+  {
+    "revision": "89895349173fc03e4ae2",
+    "url": "/homepage/static/js/2.40cfec7f.chunk.js"
+  },
+  {
+    "revision": "03664f08820fb46832d2",
+    "url": "/homepage/static/css/main.f2fec5ab.chunk.css"
+  },
+  {
+    "revision": "af4afe479c86af3860119f6c671e64e1",
+    "url": "/homepage/index.html"
+  }
+];
